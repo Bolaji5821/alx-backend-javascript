d547058ea4d0c2106b9ef4f019efe4b1794593d8
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-12-19 18:27:47 -0800

    blocked scope variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
-export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
+export default function taskBlock (trueOrFalse) {
+  const task = false
+  const task2 = true
 
-    if (trueOrFalse) {
-        let task = true;
-        let task2 = false;
-    }
-    return [task, task2];
+  if (trueOrFalse) {
+    const task = true
+    const task2 = false
+  }
+
+  return [task, task2]
 }
