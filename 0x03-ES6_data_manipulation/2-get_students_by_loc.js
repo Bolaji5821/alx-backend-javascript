8a6b3b10ba56b8e5837877fd79955110f6b9cdbd
Bolaji5821 <oyedelebolaji84@gmail.com>
2023-01-04 15:31:51 +0100

    return locaction from obj with filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,17 @@
+/**
+ * Retrieves students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
+ * @author Bolaji Oyedele <https://github.com/Bolaji5821>
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
