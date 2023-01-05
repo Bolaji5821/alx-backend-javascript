e0151b9ee00632c34dff9373d1077d81a35b7967
Bolaji5821 <oyedelebolaji84@gmail.com>
2023-01-05 04:11:17 +0100

    returns a Set from an array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Bolaji Oyedele <https://github.com/Bolaji5821>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
