4a73d6ddcfcf6be3115c07dad0a42a145b036561
Bolaji581 <oyedelebolaji84@gmail.com>
2023-02-26 22:16:43 +0100

    read file synchronously

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,22 +5,27 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  try {
-    let data = fs.readFileSync(path, 'utf8').toString().split('\n');
-    data = data.slice(1, data.length - 1);
-    console.log(`Number of students: ${data.length}`);
-    const obj = {};
-    data.forEach((el) => {
-      const student = el.split(',');
-      if (!obj[student[3]]) obj[student[3]] = [];
-      obj[student[3]].push(student[0]);
+  const promise = (res, rej) => {
+    fs.readFile(path, (err, data) => {
+      if (err) rej(Error('Cannot load the database'));
+      if (data) {
+        let newData = data.toString().split('\n');
+        newData = newData.slice(1, newData.length - 1);
+        console.log(`Number of students: ${newData.length}`);
+        const obj = {};
+        newData.forEach((el) => {
+          const student = el.split(',');
+          if (!obj[student[3]]) obj[student[3]] = [];
+          obj[student[3]].push(student[0]);
+        });
+        for (const cls in obj) {
+          if (cls) console.log(`Number of students in ${cls}: ${obj[cls].length}. List: ${obj[cls].join(', ')}`);
+        }
+      }
+      res();
     });
-    for (const cls in obj) {
-      if (cls) console.log(`Number of students in ${cls}: ${obj[cls].length}. List: ${obj[cls].join(', ')}`);
-    }
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+  };
+  return new Promise(promise);
 }
 
 module.exports = countStudents;
