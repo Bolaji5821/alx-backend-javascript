2decf06d5324a138703fff54b8680945ed5b2931
Bolaji581 <oyedelebolaji84@gmail.com>
2023-02-27 02:59:48 +0100

    complex server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,13 +5,12 @@
 
 const http = require('http');
 
-
 const app = http.createServer((req, res) => {
-	res.statusCode = 200
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello Holberton School!');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
 
-module.export = app;
+module.exports = app;
