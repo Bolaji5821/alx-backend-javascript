97552ad5f0463e7b9cd8df8fdf9da8060cf52826
Bolaji5821 <oyedelebolaji84@gmail.com>
2024-04-17 13:11:47 +0100

    test code

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,3 +1,3 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import { taskFirst, taskNext } from './0-constants.js';
 
-console.log(getBudgetObject(400, 700, 900));
+console.log(`${taskFirst()} ${taskNext()}`);
